8b88556e2fb99ee09faa598bd312c4c5efa5b2b5
bookingcliffpo-spec <bookingcliffpo@gmail.com>
2026-06-11 20:42:50 -0400

    Create index.html style.css script.js

---
+++ index.html style.css script.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Harlem Empire</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<canvas id="game"></canvas>
+<script src="script.js"></script>
+</body>
+</html>
